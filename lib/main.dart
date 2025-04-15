7e52980b5f9a11d7db0816cdb2c77ece476e3f88
AbhishekValiyapurakkal <abhiv6160@gmail.com>
2025-04-15 21:25:47 +0530

    e

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:giggles_safer_web/About_us/About_us.dart';
 import 'package:giggles_safer_web/Home/Home.dart';
-import 'package:giggles_safer_web/Our_network/our_network.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: OurNetwork());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: homePage());
   }
 }
